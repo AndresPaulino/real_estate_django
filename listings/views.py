0fbbaec9783992a970f406a5fd96a730410e4ad9
Kykeon <paulinoandresmiguel@gmail.com>
2023-02-12 14:04:33 -0500

    Add next and previous pagination function. Add order by list date and only show if properties are published

--- listings/views.py
+++ listings/views.py
@@ -4,9 +4,9 @@ from django.core.paginator import Paginator
 from .models import Listing
 
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     
-    paginator = Paginator(listings, 6)
+    paginator = Paginator(listings, 3)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
     
